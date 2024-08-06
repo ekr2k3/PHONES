80996bd6279882f7cffc8f74df33daeac038e474
ekr2k3 <133838083+ekr2k3@users.noreply.github.com>
2024-08-06 20:22:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ configDataBase();
 var prefix = require("./system").prefix;
 app.locals.prefix = prefix;
 var path = require("path");
-app.locals.pathDot = ". = " + path.resolve(".");
-app.locals.pathDirname = "__dirname = " + path.resolve(__dirname);
+app.locals.pathDot = "    . = " + path.resolve(".");
+app.locals.pathDirname = " __dirname = " + path.resolve(__dirname);
 useRouter(app);
 useRouterAdmin(app);
-app.locals.now = process.cwd();
-app.locals.dirname = __dirname;
+app.locals.now =  "     process.cwd()= " + process.cwd();
+app.locals.dirname = "   __dirname"    + __dirname;
 app.listen(port, ()=>{
     console.log("server run by port: " + port); 
 });
